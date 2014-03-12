727ec2a0beb6c623bf045557af5f8ff48db04272
Bruno Henrique <bruno_vky@live.com>
2014-03-12 10:28:52 -0300

    Parte 3

--- PyCharmScripts/second.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class ReceiveHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Fui redirecionado!')
-
-app = webapp2.WSGIApplication([
-    ('/resp', ReceiveHandler)
-], debug=True)
